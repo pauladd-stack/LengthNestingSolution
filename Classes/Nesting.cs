bd17d1c1b94160565443209d58bb3c598dbff791
Paul Addison <pauladdison@csfco.com>
2022-09-27 14:38:07 -0500

    9.27.22 Commit

--- Classes/Nesting.cs
+++ Classes/Nesting.cs
@@ -33,8 +33,76 @@ namespace LengthNestingSolution.Classes
             }
         }
 
-        
-       
 
+        /* arr[] ---> Input Array
+        data[] ---> Temporary array to
+                    store current combination
+        start & end ---> Starting and Ending
+                            indexes in arr[]
+        index ---> Current index in data[]
+        r ---> Size of a combination
+                to be printed 
+        static void combinationUtil(int[] arr, int[] data,
+                                    int start, int end,
+                                    int index, int r)
+        {
+            // Current combination is
+            // ready to be printed,
+            // print it
+            if (index == r)
+            {
+                for (int j = 0; j < r; j++)
+                    Console.Write(data[j] + " ");
+                Console.WriteLine("");
+                return;
+            }
+
+            // replace index with all
+            // possible elements. The
+            // condition "end-i+1 >=
+            // r-index" makes sure that
+            // including one element
+            // at index will make a
+            // combination with remaining
+            // elements at remaining positions
+            for (int i = start; i <= end &&
+                        end - i + 1 >= r - index; i++)
+            {
+                data[index] = arr[i];
+                combinationUtil(arr, data, i + 1,
+                                end, index + 1, r);
+            }
+        }
+
+        // The main function that prints
+        // all combinations of size r
+        // in arr[] of size n. This
+        // function mainly uses combinationUtil()
+        static void printCombination(int[] arr,
+                                        int n, int r)
+        {
+            for(int z = 0; z < r; z++)
+            {
+                // A temporary array to store
+                // all combination one by one
+                int[] data = new int[z]; //r in replace of z
+
+                // Print all combination
+                // using temporary array 'data[]'
+                combinationUtil(arr, data, 0,
+                                n - 1, 0, z); //r in replace of z
+            }
+            
+        }
+
+        // Driver Code
+        static public void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int r = arr.Length - 1;
+            int n = arr.Length;
+            printCombination(arr, n, r);
+        }
+        */
     }
 }
